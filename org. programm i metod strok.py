aacded41969f7708e18c9ce7f246c74862a5b497
koostyan <165835474+koostyan@users.noreply.github.com>
2024-08-21 14:18:05 +0300

    Add files via upload

---
+++ org. programm i metod strok.py
@@ -0,0 +1,7 @@
+my_strings = input(' rfgfg ghgjkg :')
+print (len(my_strings))
+print (my_strings.upper())
+print (my_strings.lower())
+print (my_strings.replace(' ',''))
+print (my_strings[0])
+print (my_strings[-1])

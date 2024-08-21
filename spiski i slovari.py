77f80cbd624d5ecd56365b3f27f746fed5ea4fe9
koostyan <165835474+koostyan@users.noreply.github.com>
2024-08-21 14:18:24 +0300

    Add files via upload

---
+++ spiski i slovari.py
@@ -0,0 +1,15 @@
+my_list=['banan','Aple','orange','grape','kiwi'] #список
+print(my_list)
+print(my_list[0])
+print(my_list[-1])
+print(*my_list[2:5])
+my_list[2]= False
+print(my_list)
+
+print()
+
+my_dict={'apple':'яблоко','banana':'банан','orange':'апельсин'}
+print(my_dict)
+print(my_dict['apple'])
+my_dict['banana']='небанан'
+print(my_dict)
